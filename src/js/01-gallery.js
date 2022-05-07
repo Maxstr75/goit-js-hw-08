b217cb33c71ed740b5ecdf0b3b5eec297abad6cc
Maxstr75 <91918033+Maxstr75@users.noreply.github.com>
2022-05-07 16:08:28 +0300

    add task 1 start

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const itemMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', itemMarkup);
+
+// galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+    
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems.map(({preview, original, description}) => {
+    return `
+    <div class="gallery__item">
+        <a class="gallery__item" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>
+    `;
+  })
+    .join('');
+}
+
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function (e) {
+    gallery.options.fadeSpeed = 250;
+    gallery.options.captionsData = 'alt';
+});
