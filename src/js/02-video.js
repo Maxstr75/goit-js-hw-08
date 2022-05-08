3ffa92dad9cb570c7d61ba9279513fc8da4ff159
Maxstr75 <91918033+Maxstr75@users.noreply.github.com>
2022-05-08 03:18:47 +0300

    add task 2 pravki

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,13 @@
+import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
-    const STORAGE_KEY_PLAYTIME = "videoplayer-current-time";
-    player.on('timeupdate', throttle(onPlay, 1000));
-    function onPlay(data){
-        const timePlayed = JSON.stringify(data.seconds);
-        localStorage.setItem(STORAGE_KEY_PLAYTIME, timePlayed);
-    };
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
-    setCurrentTime();
-    function setCurrentTime(){
-        const savedTime = localStorage.getItem(STORAGE_KEY_PLAYTIME);
-        if(savedTime){
-            const convertSavedTime = JSON.parse(savedTime);
-            player.setCurrentTime(convertSavedTime);
-        };
+const player = new Vimeo.Player(iframe);
+const STORAGE_KEY = 'videoplayer - current - time';
+const onPlay = function (data) {
+  // data is an object containing properties specific to that event
+  localStorage.setItem(STORAGE_KEY, data.seconds);
+  console.log(data);
 };
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(30.456);
